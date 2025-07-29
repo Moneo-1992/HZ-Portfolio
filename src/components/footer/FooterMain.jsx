3897b9eb0b36c0f7e7887966349a29b94f2ff8f8
ZhangHe <moneo1217@gmail.com>
2025-07-29 15:38:34 +0800

    fix

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -69,7 +69,7 @@ const FooterMain = () => {
         </ul>
       </div>
       <p className="max-w-[1200px] mx-auto text-right mt-2 mb-12 text-sm text-lightBrown">
-        © 2024 Abdelrahman | All Rights Reserved.
+        © 2025 Heath | All Rights Reserved.
       </p>
     </div>
   );
--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
   
   return (
     <div className='flex items-center justify-center'>
-      <h1 className="text-white text-lg sm:text-lg md:text-2xl sm:block md:block">
+      <h1 className="text-white text-lg sm:text-sm md:text-2xl sm:block md:block">
         {t('welcome')}
       </h1>
       
